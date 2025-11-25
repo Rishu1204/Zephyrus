4de2d4a64f42c02a8945764275d1b479ce8a1bd4
RishuRaj <rishurajcuchd@gmail.com>
2025-11-25 19:55:09 +0530

    initial commit

---
+++ src/main/java/com/example/zephyrus/ZephyrusApplication.java
@@ -0,0 +1,13 @@
+package com.example.zephyrus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZephyrusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZephyrusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/zephyrus/ZephyrusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zephyrus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZephyrusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
